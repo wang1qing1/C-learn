606894586cac645a5949c9e658cce99aa8ac4ee0
满身欲望 <3521369046@qq.com>
2022-10-11 10:26:26 +0800

    教务系统—文件版本，使用文件控制输入的数据到磁盘文件中达到长久保存，也可以从文件中读取数据。

---
+++ 教务系统—文件版本/Quence.h
@@ -0,0 +1,41 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+#define INIT_NUM 3
+typedef struct Stu
+{
+	char name[30];
+	char ID[20];
+	int age;
+}Stu;
+typedef struct Quence
+{
+	int Initnum;
+	int Nownum;
+	Stu* stu;
+}Que;
+
+Que*InitQue();
+
+void InQue(Que*quence);
+
+void Prin(Que*que);
+
+void menu();
+
+void Dele(Que* que);
+
+void Find(Que* que);
+
+void Modify(Que* que);
+ 
+void Destroy(Que*que);
+
+int Stucmp_ID(const void* e1, const void* e2);
+
+void insert(Que* que);
+
+void Sort(Que* que);
